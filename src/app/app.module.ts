856e21abc6dea63a7927b9ec39736388ec3e6558
sengvang1 <79110702+sengvang1@users.noreply.github.com>
2022-04-26 17:28:40 -0500

    Latest

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import {Routes, RouterModule} from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
@@ -31,7 +32,8 @@ const appRoutes: Routes = [
   imports: [
     BrowserModule,
     HttpClientModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    FormsModule
   ],
   providers: [
     HousingService,
--- src/app/property/add-property/add-property.component.ts
+++ src/app/property/add-property/add-property.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgForm } from '@angular/forms'
 
 @Component({
   selector: 'app-add-property',
@@ -7,7 +8,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./add-property.component.css']
 })
 export class AddPropertyComponent implements OnInit {
-
+@ViewChild('Form') addPropertyForm: NgForm;
   constructor(private router: Router) { }
 
   ngOnInit() {
@@ -16,6 +17,12 @@ export class AddPropertyComponent implements OnInit {
   onBack()
   {
     this.router.navigate(['/']);
+  }
+
+  onSubmit()
+  {
+    console.log('Submitted');
+    console.log(this.addPropertyForm);
   }
 
 }
