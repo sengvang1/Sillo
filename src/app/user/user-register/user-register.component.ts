6e22262f6a6f6c36c7d5078093537fb53a215502
sengvang1 <79110702+sengvang1@users.noreply.github.com>
2022-04-28 17:38:30 -0500

    login form

--- src/app/user/user-register/user-register.component.ts
+++ src/app/user/user-register/user-register.component.ts
@@ -14,13 +14,37 @@ export class UserRegisterComponent implements OnInit {
 
   ngOnInit() {
     this.registrationForm = new FormGroup({
-      userName: new FormControl('Mark', Validators.required),
-      email: new FormControl(null, [Validators.required, Validators.email])
-    });
+      userName: new FormControl(null, Validators.required),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+      confirmPassword: new FormControl(null, [Validators.required]),
+      mobile: new FormControl(null, [Validators.required, Validators.maxLength(10)])
+    }, this.passwordMatchingValidator);
+  }
+
+  passwordMatchingValidator(fg: FormGroup) : Validators {
+    return fg.get('password').value === fg.get('confirmPassword').value ? null : {notmatched: true};
   }
 
   onSubmit() {
     console.log(this.registrationForm);
+  }
+
+  // Getter methods for all form controls
+  get userName() {
+    return this.registrationForm.get('userName') as FormControl;
+  }
+  get email() {
+    return this.registrationForm.get('email') as FormControl;
+  }
+  get password() {
+    return this.registrationForm.get('password') as FormControl;
+  }
+  get confirmPassword() {
+    return this.registrationForm.get('confirmPassword') as FormControl;
+  }
+  get mobile() {
+    return this.registrationForm.get('mobile') as FormControl;
   }
 
 }
